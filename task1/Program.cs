a2579327c8d755bb3fab5486a42f3d51edf9e284
Елена <egoncharova72@yandex.ru>
2023-01-09 22:08:44 +0400

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,32 @@
-﻿// 
-Console.WriteLine("Hello,");
+﻿//Написать программу,которая из  имеющегося массива строк формирует
+//  массив из строк,длина которых меньше либо равна 3 символам
+
+
+Console .Write ( " ввдедите значения в консоль через пробел : ");
+ 
+ string[] massiv =Console. ReadLine().Split  (" ") ;
+string[] SecondArray(string[] arr)
+{
+    int count = 0;
+    string[] newarr = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+    if(arr[i].Length <= 3)
+        {
+        newarr[count] = arr[i];
+        count++;
+        }
+    }
+    return newarr;
+}
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i] } " );
+    }
+    Console.WriteLine("]");
+}
+ string[]secondArray= SecondArray(massiv );
+PrintArray(secondArray);
